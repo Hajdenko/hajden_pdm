782a3698564f6f218438b5aa8cf104e1a17dc1b7
Hajden <138399356+Hajdenko@users.noreply.github.com>
2024-10-10 17:13:38 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -64,6 +64,8 @@ Config.TestDrive = {
 
 
 -- SERVER CONFIG --
+ESX = exports["es_extended"]:getSharedObject()
+
 Config.playerVehiclesTable = "owned_vehicles" -- qbcore: player_vehicles
 
 Config.getPlayer = function(source)
@@ -93,6 +95,16 @@ end
 Config.addMoney = function(source, amount)
     Config.getPlayer(source).addMoney(amount)
 end
+
+Config.vehiclePlatePlate = ESX.GetConfig().CustomAIPlates or "HEY ..." -- Custom plates for AI vehicles
+-- Pattern string format
+--1 will lead to a random number from 0-9.
+--A will lead to a random letter from A-Z.
+-- . will lead to a random letter or number, with a 50% probability of being either.
+--^1 will lead to a literal 1 being emitted.
+--^A will lead to a literal A being emitted.
+--Any other character will lead to said character being emitted.
+-- A string shorter than 8 characters will be padded on the right.
 
 Config.Notify = function(source, message)
     TriggerClientEvent('ox_lib:notify', source, {
